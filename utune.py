1a5ab4780d74b810743df08d3ac1be127dba6ddc
manidesto <manideepiitb@gmail.com>
2015-05-29 16:11:58 +0530

    Adds metadata existing mp3 as well

--- utune.py
+++ utune.py
@@ -19,6 +19,7 @@ def add_metadata(audiofile, metafile=None):
     metadata = Metamine(metafile).get_metadata()
     print(json.dumps(metadata, indent = 4))
     audiofile = eyed3.load(audiofile)
+    audiofile.tag.version = eyed3.id3.ID3_V2_4
     audiofile.tag.title = metadata['title']
     audiofile.tag.artist = metadata['artist']
     audiofile.tag.album_artist = metadata['album_artist']
@@ -61,9 +62,10 @@ def download_audio(link):
 
 def main(argv):
     metafile = None
+    song = None
     link = None
     try:
-        opts, args = getopt.getopt(argv, 'm:l:', ["metadata=", "link="])
+        opts, args = getopt.getopt(argv, 'm:s:l:', ["metadata=", "song=", "link="])
     except getopt.GetoptError as e:
         print('Something went wrong %s' % e)
         return 2
@@ -72,14 +74,19 @@ def main(argv):
         if opt in ('-m', '--metadata'):
             print('metafile = ' + arg)
             metafile = arg
+        elif opt in ('-s', '--song'):
+            song = arg
         elif opt in ('-l', '--link'):
             link = arg
 
-    if link is None:
-        link = raw_input('link: ')
+    if song is None:
+        if link is None:
+            link = raw_input('link: ')
 
-    print('downloading audio')
-    temp_audio = download_audio(link)
+        print('downloading audio')
+        temp_audio = download_audio(link)
+    else:
+        temp_audio = './' + song
 
     print('Adding metadata')
     title = add_metadata(temp_audio, metafile)
